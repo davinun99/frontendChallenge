c7ec042d3fb10b510b310d54f252fda4ff022cac
David <nundavid99@gmail.com>
2023-07-26 18:46:40 -0400

    Add eslint deps & fix problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import logo from './around-logo.webp';
-import './App.css';
+import React from 'react'
+import logo from './around-logo.webp'
+import './App.css'
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="breathing-animation" alt="logo" />
+        <img alt="logo" className="breathing-animation" src={logo} />
         <p>
           Check <code>README.md</code> file
         </p>
-        
+
       </header>
     </div>
-  );
-}
+)
 
-export default App;
+export default App
