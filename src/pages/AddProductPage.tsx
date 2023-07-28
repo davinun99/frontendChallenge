f74be8b3f6b9d69c57fc141fcad029acc055b139
David <nundavid99@gmail.com>
2023-07-28 18:59:55 -0400

    add product page & form

---
+++ src/pages/AddProductPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import PageTitle from 'src/components/PageTitle'
+import AddProductForm from 'src/components/AddProductForm'
+
+const AddProductPage = () => (
+  <>
+    <PageTitle title='Add new product' />
+    <AddProductForm />
+  </>
+)
+export default AddProductPage
