9eea22f10a2e42a7412d4d3dd5d7a6826bb22ff3
David <nundavid99@gmail.com>
2023-07-28 16:26:54 -0400

    reviews and icons with styling

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -10,14 +10,14 @@ const ProductList: React.FC<Props> = ({ data }) => {
   console.log('Review list', { data })
   return (
     <main className="container">
-        <h3 className='shadow-sm rounded p-3 my-4 text-center'>Products</h3>
+        <h3 className='shadow rounded p-3 my-5 text-center'>Products</h3>
         <ul className='list-unstyled row'>
           {!!data.queryItem &&
             data.queryItem.map(
               item =>
                 !!item && (
-                <li key={`review-${item.name}`} className="col-12 col-md-6 mb-3">
-                  <div className='around-card row g-1 shadow-sm p-3'>
+                <li key={`review-${item.name}`} className="col-12 col-md-6 p-0 p-md-2 p-lg-3">
+                  <div className='around-card row g-1 p-3'>
                     <div className='col-4'>
                       <img alt={item.name} className='item-img' src={item.img || 'https://upload.wikimedia.org/wikipedia/commons/0/0a/No-image-available.png'}/>
                     </div>
--- src/components/ReviewList.tsx
+++ src/components/ReviewList.tsx
@@ -11,27 +11,32 @@ const ReviewList: React.FC<Props> = ({ data }) => {
   return (
     <main className="container">
         <h3 className='shadow-sm rounded p-3 mt-3 text-center'>Reviews</h3>
-        <ul className='list-unstyled row'>
+        <ul className='list-unstyled row mt-3'>
           {!!data.queryReview &&
             data.queryReview.map(
               (review, i) =>
                 !!review && (
-                <li key={`review-${review.id}`} className="col-12 col-md-6 mb-2">
-                  <div className='around-card row g-1'>
-                    <div className='col-4'>
-                      <img alt={review.item?.name} src={review.item?.img || 'https://upload.wikimedia.org/wikipedia/commons/0/0a/No-image-available.png'}/>
-                    </div>
-                    <div className='col-8'>
-                      <h6 className='bold'><b>{review.item?.name}</b></h6>
-                      {/* ({review.text}) */}
-                      <b>Description</b>
-                      <p>
-                        {review.item?.description}
-                      </p>
-                      <span>
-                        <b>Price:</b> ${review.item?.price}
+                <li key={`review-${review.id}`} className="col-12 col-md-6 col-lg-4 mb-2">
+                  <div className='review-card h-100'>
+                    <div className='d-flex align-items-center'>
+                      <img alt='Reviewer' className='reviewer-photo' src='https://images-na.ssl-images-amazon.com/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png' />
+                      <span className='ms-2'>
+                        John Doe
+                        {Math.random() > 0.6 && <i className="reviewer-check bi bi-check-circle-fill" />}
                       </span>
                     </div>
+                    <p className='my-0 text-secondary'>{review.item?.name || 'Unknown item :o'}</p>
+                    <div className='d-flex'>
+                      <ul className='list-unstyled stars-list me-1'>
+                        <li><i className="bi bi-star" /></li>
+                        <li><i className="bi bi-star" /></li>
+                        <li><i className="bi bi-star" /></li>
+                        <li><i className="bi bi-star" /></li>
+                        <li><i className="bi bi-star" /></li>
+                      </ul>
+                      <b className='review-title'>The best phone!</b>
+                    </div>
+                    {review.text}
                   </div>
                 </li>
                 )
