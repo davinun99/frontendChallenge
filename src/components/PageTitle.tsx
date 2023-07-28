a08301b8c906aa11c2d7b58424635e9d9e8b0260
David <nundavid99@gmail.com>
2023-07-28 18:34:00 -0400

    Loader & page title components

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PageTitle = ({ title }: {title: string}) => (
   <header className='container'>
-    <h3 className='shadow-sm rounded p-3 mt-5 mb-4 text-center'>{title}</h3>
+    <h3 className='shadow-sm rounded p-3 mt-5 text-center'>{title}</h3>
   </header>
 )
 export default PageTitle
