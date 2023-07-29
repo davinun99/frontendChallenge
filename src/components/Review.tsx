daa04a078736b2fc0e44b149f2a28ce06d72a008
David <nundavid99@gmail.com>
2023-07-28 22:09:19 -0400

    review component separated

---
+++ src/components/Review.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Review as ReviewType } from 'src/types'
+import { faker } from '@faker-js/faker'
+
+const Review = ({ review }: {review: ReviewType}) => (
+  <li className="col-12 col-md-6 col-lg-4 mb-2">
+    <div className='review-card h-100'>
+      <div className='d-flex align-items-center'>
+        <img alt='Reviewer' className='reviewer-photo' src={faker.image.avatar()} />
+        <span className='ms-2'>
+          {faker.person.fullName()}
+          {Math.random() > 0.6 && <i className="reviewer-check bi bi-check-circle-fill" />}
+        </span>
+      </div>
+      <p className='my-0 text-secondary'>{review.item?.name || 'Unknown item :o'}</p>
+      <div className='d-flex'>
+        <ul className='list-unstyled stars-list me-1'>
+          <li><i className="bi bi-star" /></li>
+          <li><i className="bi bi-star" /></li>
+          <li><i className="bi bi-star" /></li>
+          <li><i className="bi bi-star" /></li>
+          <li><i className="bi bi-star" /></li>
+        </ul>
+        <b className='review-title'>{faker.lorem.words(3)}</b>
+      </div>
+      {review.text}
+    </div>
+  </li>
+)
+
+export default Review
--- src/components/ReviewList.tsx
+++ src/components/ReviewList.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import * as React from 'react'
 import { GraphQlReview } from 'src/types'
-import { faker } from '@faker-js/faker'
 import { Container } from 'react-bootstrap'
+import Review from './Review'
 
 interface Props {
   data: GraphQlReview;
@@ -15,29 +15,7 @@ const ReviewList: React.FC<Props> = ({ data }) => (
         data.queryReview.map(
           (review, i) =>
             !!review && (
-            <li key={`review-${review.id}`} className="col-12 col-md-6 col-lg-4 mb-2">
-              <div className='review-card h-100'>
-                <div className='d-flex align-items-center'>
-                  <img alt='Reviewer' className='reviewer-photo' src={faker.image.avatar()} />
-                  <span className='ms-2'>
-                    {faker.person.fullName()}
-                    {Math.random() > 0.6 && <i className="reviewer-check bi bi-check-circle-fill" />}
-                  </span>
-                </div>
-                <p className='my-0 text-secondary'>{review.item?.name || 'Unknown item :o'}</p>
-                <div className='d-flex'>
-                  <ul className='list-unstyled stars-list me-1'>
-                    <li><i className="bi bi-star" /></li>
-                    <li><i className="bi bi-star" /></li>
-                    <li><i className="bi bi-star" /></li>
-                    <li><i className="bi bi-star" /></li>
-                    <li><i className="bi bi-star" /></li>
-                  </ul>
-                  <b className='review-title'>{faker.lorem.words(3)}</b>
-                </div>
-                {review.text}
-              </div>
-            </li>
+              <Review key={review.id} review={review} />
             )
         )}
     </ul>
